b34742f54f93931902d05a8555b169933a2d81bf
machen2 <victoriamachen@gmail.com>
2017-07-18 09:25:32 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,13 +4,13 @@ class CartsController < ApplicationController
   end
 
   def checkout
-  #  @cart = Cart.find(params[:id])
-  #  @cart.cart_checkout
     current_cart.line_items.each do |line_item|
       line_item.item.inventory -= line_item.quantity
       line_item.item.save
     end
-    @current_user.current_cart = nil
-    redirect_to cart_path(@current_user.current_cart_id)
+    @cart = Cart.find_by(id: @current_user.current_cart_id)
+    @cart.destroy
+    #redirect_to cart_path(@current_user.current_cart_id)
+    redirect_to root_path
   end
 end
